838738f63c2c7d9444a95157934b7263ecec1466
rashidpaha <rashidpasha@mail.ru>
2022-12-22 20:44:37 +0300

    Задание 1 Семинар 1

---
+++ seminar_1_task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+int result = b = b * 2;
+
+if (a == b)
+{
+    Console.WriteLine("Является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Не является квадратом второго числа");
+}
