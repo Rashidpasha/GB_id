6bc858e3d14059e407900515c3ee8f8927dbaa1a
rashidpaha <rashidpasha@mail.ru>
2022-12-23 17:11:14 +0300

    Задание 4 Семинар 1

---
+++ seminar_1_task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Выяснить является ли число чётным
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0);
+{
+    Console.WriteLine("Является четным числом.");
+}
+else
+{
+    Console.WriteLine("Не являеется четным числом.");
+}
