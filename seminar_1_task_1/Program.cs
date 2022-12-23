aac5577ddf5e42dcc68f1214ad40048139d94b29
rashidpaha <rashidpasha@mail.ru>
2022-12-23 17:18:13 +0300

    Задание 1 Семинар 1

--- seminar_1_task_1/Program.cs
+++ seminar_1_task_1/Program.cs
@@ -6,8 +6,6 @@ int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число ");
 int b = int.Parse(Console.ReadLine()!);
 
-//int result = b = b * 2;
-
 if (a/b == b)
 {
     Console.WriteLine("Является квадратом второго числа");
