90fd7ac6a01ba886670774a411921f3ba3bc6ccc
rashidpaha <rashidpasha@mail.ru>
2023-01-04 20:57:44 +0300

    Домашнее задание семинар 2 задание 15

---
+++ seminar_2_fask_15/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 15.
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+Console.Write("Введите число номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+if(day == a)
+{
+    Console.WriteLine ("понедельник");
+    Console.WriteLine ("Будний день!");
+}
+
+if(day == b)
+{
+    Console.WriteLine ("вторник");
+    Console.WriteLine ("Будний день!");
+}
+if(day == c)
+{
+    Console.WriteLine ("среда");
+    Console.WriteLine ("Будний день!");
+}
+if(day == d)
+{
+    Console.WriteLine ("четверг");
+    Console.WriteLine ("Будний день!");
+}
+if(day == e)
+{
+    Console.WriteLine ("пятница");
+    Console.WriteLine ("Будний день!");
+}
+if(day == f)
+{
+    Console.WriteLine ("суббота");
+    Console.WriteLine ("Ура!выходной!");
+}
+if(day == g)
+{
+    Console.WriteLine ("воскресение");
+    Console.WriteLine ("Ура!выходной!");
+}
