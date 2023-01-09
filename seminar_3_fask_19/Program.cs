cf6714a9d445a00123cf5e0dabc77822569c4fac
rashidpaha <rashidpasha@mail.ru>
2023-01-09 17:26:08 +0300

    Домашнее задание семинар 2 задание 19

---
+++ seminar_3_fask_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №19.
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+int b = number.Length;
+
+if (b == 5 )
+
+if ( number[0] == number[4] && number[1] == number[3])
+Console.WriteLine($"{number} - Ураа!Палиндром!");
+
+else
+Console.Write("Это не палиндром.");
+
+else 
+Console.Write("Введите еще раз ваше число! ");
