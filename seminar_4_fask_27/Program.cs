47f1565a2e01ee5304bdc4f03ea2e7be5c84ae86
rashidpaha <rashidpasha@mail.ru>
2023-01-10 17:41:43 +0300

    Домашнее задание семинар 4 задание 27

---
+++ seminar_4_fask_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №27.
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.Write("Введите число -> ");
+int x = int.Parse(Console.ReadLine()!);
+string string_x = Convert.ToString(x);
+
+int summ = 0;
+
+for (int i = 0; i < string_x.Length; i++)
+{
+   string a = Convert.ToString(string_x[i]);
+   summ = summ + Convert.ToInt32(a);
+}
+Console.Write(summ);
